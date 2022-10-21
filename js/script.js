2a16621ac2bbb2bdbfb9a8f90a7091e79cb88266
iskandar <isikjandev@gmail.com>
2022-10-21 19:05:32 +0500

    mobile

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-
     $('#colorpicker').farbtastic('#color');
-
+});
+$("#settings").on("mouseenter mouseleave", function(event) {
+    $(".slideup__settings").toggleClass("slideup__settings-active");
 });
